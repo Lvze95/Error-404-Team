f65b389dff84db00fa8826d2720fb3897e08af79
Lazar <50754397+Lvze95@users.noreply.github.com>
2022-06-10 14:14:39 +0200

    Update main.js
    Removing comments

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
 $(document).ready(function() {
     $('.loader-wrapper').delay(2000).fadeOut('slow');
 });
-
-// $(document).ready(function(){
-//     $("#left1").hide();
-//     setTimeout(function () {
-//         $("#left1").show();
-//     }, 2000);
-// });
-// $(document).ready(function(){
-//     $("#right1").hide();
-//     setTimeout(function () {
-//         $("#right1").show();
-//     }, 2000);
-// });
